71492d5fa2779d53bd9038173f86b231e67b5857
MarvinL.com <contact@marvinl.com>
2024-09-05 22:03:32 -0400

    chore: update prettier config, add twitter api v2

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,9 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
 };
 
 export default config;
